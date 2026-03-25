511812abe9406be5a21de75c6fcd38ba3be56c4e
theo <theoemail@internet.ru>
2026-03-25 17:08:41 +0300

    refatoring

--- pages/basePage.ts
+++ pages/basePage.ts
@@ -17,38 +17,11 @@ export abstract class BasePage {
             .toBeVisible();
     }
 
-    async waitForUrl(re: RegExp) {
-        await expect(this.page).toHaveURL(re);
-    }
-
-    async clickAndWaitForUrl(clickTarget: Locator, re: RegExp) {
-        await Promise.all([
-            this.waitForUrl(re),
-            clickTarget.click(),
-        ]);
-    }
-
     async fill(locator: Locator, value: string) {
         await locator.fill(value);
     }
 
     async click(locator: Locator) {
         await locator.click();
-    }
-
-    async expectVisible(locator: Locator) {
-        await expect(locator).toBeVisible();
-    }
-
-    async expectHidden(locator: Locator) {
-        await expect(locator).toBeHidden();
-    }
-
-    async expectEnabled(locator: Locator) {
-        await expect(locator).toBeEnabled();
-    }
-
-    async expectDisabled(locator: Locator) {
-        await expect(locator).toBeDisabled();
     }
 }
